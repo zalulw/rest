4448f5401d1e61f176d7b9aedd339075ed22c6ba
zalulw <polyaakzalan394@gmail.com>
2025-09-05 11:32:41 +0200

    hazi

---
+++ homework 1/homework/homework/Controllers/R6OpsController.cs
@@ -0,0 +1,89 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace homework.Controllers;
+
+public class R6OpsController : ControllerBase
+{
+    public List<string> Operators = new List<string>()
+    {
+        "Denari",
+        "Rauora",
+        "Striker",
+        "Deimos",
+        "Ram",
+        "Brava",
+        "Grim",
+        "Sens",
+        "Osa",
+        "Flores",
+        "Zaro",
+        "Ace",
+        "Iana",
+        "Kali",
+        "Amaru",
+        "Nokk",
+        "Gridlock fatass",
+        "Nomad",
+        "Maverick",
+        "Lion",
+        "Finka",
+        "Dokkaebi",
+        "Zofia",
+        "Ying",
+        "Jackal",
+        "Hibana",
+        "Capitao",
+        "Buck",
+        "Sledge",
+        "Thatcher",
+        "Ash",
+        "Thermite",
+        "Montagne",
+        "Twitch",
+        "Blitz my king",
+        "IQ",
+        "Fuze",
+        "Glaz"
+    };
+
+    [HttpGet]
+    [Route("operators/id/{id}")]
+    public async Task<IActionResult> GetByIdAsync([FromRoute][Required] int id)
+    {
+        return Ok(Operators[id]);
+    }
+    [HttpGet]
+    [Route("operators/all")]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        return Ok(Operators);
+    }
+
+    [HttpPost]
+    [Route("operators/create")]
+    public async Task<IActionResult> PostAsync([FromBody][Required] string op) 
+    {
+        Operators.Add(op);
+        return Ok(Operators);
+    }
+
+    [HttpPut]
+    [Route("operators/update")]
+    public async Task<IActionResult> UpdateAsync([FromBody][Required] OpUpdateModel request)
+    {
+        Operators[request.Id] = request.Name;
+
+        return Ok(Operators);
+    }
+
+    [HttpDelete]
+    [Route("operators/delete")]
+    public async Task<IActionResult> DeleteAsync([FromRoute][Required] int id)
+    {
+        Operators.RemoveAt(id);
+
+        return Ok(Operators);
+    }
+
+}
